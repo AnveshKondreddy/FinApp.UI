727c8da012fe0ac5021b7bc291ff6d3638f1dd89
AnveshKondreddy <kondreddyanvesh.coder@gmail.com>
2018-09-29 21:38:43 -0700

    Initial Checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FinApp';
+  value: Date;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import {CalendarModule} from 'primeng/calendar'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,BrowserAnimationsModule,
+    FormsModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
