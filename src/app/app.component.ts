a92e00da8443b8b12fefc816909b70ee1bc65c93
AnveshKondreddy <kondreddyanvesh.coder@gmail.com>
2018-10-02 14:30:13 -0700

    Initial Menubar chekin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FinApp';
-  value: Date;
+  items: MenuItem[];
+
+    ngOnInit() {
+      this.items = [
+        {
+            label: 'Home',
+            icon: 'fa fa-home fa-2x',
+            routerLink:'home'
+        },
+        {
+          label:'Funds',
+          icon:'fa fa-suitcase fa-2x',
+          routerLink:'#'
+        },
+        {
+          label:'Members',
+          icon:'fa fa-group fa-2x',
+          routerLink:'members'
+        },
+        {
+          label:'Admin',
+          icon:'fa fa-dashboard fa-2x',
+          items: [{
+            label: 'New', 
+            icon: 'pi pi-fw pi-plus',
+            items: [
+                {label: 'Project'},
+                {label: 'Other'},
+            ]
+        },
+        {label: 'Open'},
+        {separator:true},
+        {label: 'Quit'}
+    ]
+        }
+    ];    
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import {CalendarModule} from 'primeng/calendar'
+import { CalendarModule } from 'primeng/calendar'
 import { FormsModule } from '@angular/forms';
+import { MenubarModule } from 'primeng/menubar';
+import { MemberComponent } from './member/member.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  { path: 'members', component: MemberComponent },
+  {path:'', redirectTo:'home',pathMatch:'full'},
+  {path:'**',redirectTo:'home',pathMatch:'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemberComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule,BrowserAnimationsModule,
+    BrowserModule, BrowserAnimationsModule,
+    RouterModule.forRoot(routes, { useHash: true }),
     FormsModule,
+    MenubarModule,
     CalendarModule
   ],
   providers: [],
